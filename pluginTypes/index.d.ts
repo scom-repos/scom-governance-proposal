b7aed76bbf26f60a3f13f0085c40af3f3bbb4eec
ojochau <ojochau@instajs.com>
2023-09-20 14:42:53 +0800

    init widget

---
+++ pluginTypes/index.d.ts
@@ -0,0 +1,144 @@
+/// <reference path="@scom/scom-dapp-container/@ijstech/eth-wallet/index.d.ts" />
+/// <reference path="@ijstech/eth-wallet/index.d.ts" />
+/// <reference path="@scom/scom-token-input/@ijstech/eth-wallet/index.d.ts" />
+/// <reference path="@scom/scom-token-input/@scom/scom-token-modal/@ijstech/eth-wallet/index.d.ts" />
+/// <amd-module name="@scom/scom-governance-proposal/interface.ts" />
+declare module "@scom/scom-governance-proposal/interface.ts" {
+    import { INetworkConfig } from "@scom/scom-network-picker";
+    import { IWalletPlugin } from "@scom/scom-wallet-modal";
+    export interface IGovernanceProposal {
+        wallets: IWalletPlugin[];
+        networks: INetworkConfig[];
+        defaultChainId?: number;
+        showHeader?: boolean;
+    }
+}
+/// <amd-module name="@scom/scom-governance-proposal/assets.ts" />
+declare module "@scom/scom-governance-proposal/assets.ts" {
+    function fullPath(path: string): string;
+    const _default: {
+        fullPath: typeof fullPath;
+    };
+    export default _default;
+}
+/// <amd-module name="@scom/scom-governance-proposal/store/core.ts" />
+declare module "@scom/scom-governance-proposal/store/core.ts" {
+    export interface CoreAddress {
+        OAXDEX_Governance: string;
+        OAXDEX_VotingRegistry: string;
+        GOV_TOKEN: string;
+    }
+    export const coreAddress: {
+        [chainId: number]: CoreAddress;
+    };
+}
+/// <amd-module name="@scom/scom-governance-proposal/store/utils.ts" />
+declare module "@scom/scom-governance-proposal/store/utils.ts" {
+    import { ERC20ApprovalModel, IERC20ApprovalEventOptions, INetwork } from "@ijstech/eth-wallet";
+    export class State {
+        infuraId: string;
+        networkMap: {
+            [key: number]: INetwork;
+        };
+        rpcWalletId: string;
+        approvalModel: ERC20ApprovalModel;
+        constructor(options: any);
+        private initData;
+        initRpcWallet(defaultChainId: number): string;
+        getRpcWallet(): import("@ijstech/eth-wallet").IRpcWallet;
+        isRpcWalletConnected(): boolean;
+        getChainId(): number;
+        private setNetworkList;
+        setApprovalModelAction(options: IERC20ApprovalEventOptions): Promise<import("@ijstech/eth-wallet").IERC20ApprovalAction>;
+        getAddresses(chainId?: number): import("@scom/scom-governance-proposal/store/core.ts").CoreAddress;
+    }
+    export function isClientWalletConnected(): boolean;
+}
+/// <amd-module name="@scom/scom-governance-proposal/store/index.ts" />
+declare module "@scom/scom-governance-proposal/store/index.ts" {
+    export * from "@scom/scom-governance-proposal/store/utils.ts";
+}
+/// <amd-module name="@scom/scom-governance-proposal/data.json.ts" />
+declare module "@scom/scom-governance-proposal/data.json.ts" {
+    const _default_1: {
+        infuraId: string;
+        networks: {
+            chainId: number;
+            explorerTxUrl: string;
+            explorerAddressUrl: string;
+        }[];
+        defaultBuilderData: {
+            defaultChainId: number;
+            networks: {
+                chainId: number;
+            }[];
+            wallets: {
+                name: string;
+            }[];
+            showHeader: boolean;
+            showFooter: boolean;
+        };
+    };
+    export default _default_1;
+}
+/// <amd-module name="@scom/scom-governance-proposal/index.css.ts" />
+declare module "@scom/scom-governance-proposal/index.css.ts" {
+    const _default_2: string;
+    export default _default_2;
+}
+/// <amd-module name="@scom/scom-governance-proposal" />
+declare module "@scom/scom-governance-proposal" {
+    import { ControlElement, Module } from "@ijstech/components";
+    import { INetworkConfig } from "@scom/scom-network-picker";
+    import { IWalletPlugin } from "@scom/scom-wallet-modal";
+    import { IGovernanceProposal } from "@scom/scom-governance-proposal/interface.ts";
+    interface ScomGovernanceProposalElement extends ControlElement {
+        lazyLoad?: boolean;
+        networks: INetworkConfig[];
+        wallets: IWalletPlugin[];
+        defaultChainId?: number;
+        showHeader?: boolean;
+    }
+    global {
+        namespace JSX {
+            interface IntrinsicElements {
+                ['i-scom-governance-proposal']: ScomGovernanceProposalElement;
+            }
+        }
+    }
+    export default class GovernanceProposal extends Module {
+        private dappContainer;
+        private loadingElm;
+        private txStatusModal;
+        private mdWallet;
+        private state;
+        private _data;
+        tag: any;
+        private get chainId();
+        get defaultChainId(): number;
+        set defaultChainId(value: number);
+        get wallets(): IWalletPlugin[];
+        set wallets(value: IWalletPlugin[]);
+        get networks(): INetworkConfig[];
+        set networks(value: INetworkConfig[]);
+        get showHeader(): boolean;
+        set showHeader(value: boolean);
+        removeRpcWalletEvents(): void;
+        onHide(): void;
+        isEmptyData(value: IGovernanceProposal): boolean;
+        init(): Promise<void>;
+        private _getActions;
+        getConfigurators(): any[];
+        private getData;
+        private setData;
+        getTag(): Promise<any>;
+        private updateTag;
+        private setTag;
+        private resetRpcWallet;
+        private refreshUI;
+        private initWallet;
+        private initializeWidgetConfig;
+        private connectWallet;
+        render(): any;
+    }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,24 @@
-import { ControlElement, customElements, Module } from "@ijstech/components";
+import {
+    application,
+    ControlElement,
+    customElements,
+    Module,
+    Panel,
+    Styles
+} from "@ijstech/components";
 import ScomDappContainer from "@scom/scom-dapp-container";
 import { INetworkConfig } from "@scom/scom-network-picker";
-import { IWalletPlugin } from "@scom/scom-wallet-modal";
+import ScomTxStatusModal from '@scom/scom-tx-status-modal';
+import ScomTokenInput from '@scom/scom-token-input';
+import ScomWalletModal, { IWalletPlugin } from "@scom/scom-wallet-modal";
 import { IGovernanceProposal } from "./interface";
+import Assets from './assets';
+import { isClientWalletConnected, State } from "./store/index";
+import configData from './data.json';
+import { Constants, Wallet } from "@ijstech/eth-wallet";
+import customStyles from './index.css';
+
+const Theme = Styles.Theme.ThemeVars;
 
 interface ScomGovernanceProposalElement extends ControlElement {
     lazyLoad?: boolean;
@@ -23,15 +39,211 @@ declare global {
 @customElements('i-scom-governance-proposal')
 export default class GovernanceProposal extends Module {
     private dappContainer: ScomDappContainer;
+    private loadingElm: Panel;
+    private txStatusModal: ScomTxStatusModal;
+    private mdWallet: ScomWalletModal;
+    private state: State;
     private _data: IGovernanceProposal = {
         wallets: [],
         networks: []
     };
     tag: any = {};
 
+    private get chainId() {
+        return this.state.getChainId();
+    }
+
+    get defaultChainId() {
+        return this._data.defaultChainId;
+    }
+
+    set defaultChainId(value: number) {
+        this._data.defaultChainId = value;
+    }
+
+    get wallets() {
+        return this._data.wallets ?? [];
+    }
+    set wallets(value: IWalletPlugin[]) {
+        this._data.wallets = value;
+    }
+
+    get networks() {
+        return this._data.networks ?? [];
+    }
+    set networks(value: INetworkConfig[]) {
+        this._data.networks = value;
+    }
+
+    get showHeader() {
+        return this._data.showHeader ?? true;
+    }
+    set showHeader(value: boolean) {
+        this._data.showHeader = value;
+    }
+
+    removeRpcWalletEvents() {
+        const rpcWallet = this.state.getRpcWallet();
+        if (rpcWallet) rpcWallet.unregisterAllWalletEvents();
+    }
+
+    onHide() {
+        this.dappContainer.onHide();
+        this.removeRpcWalletEvents();
+    }
+
+    isEmptyData(value: IGovernanceProposal) {
+        return !value || !value.networks || value.networks.length === 0;
+    }
+
+    async init() {
+        this.isReadyCallbackQueued = true;
+        super.init();
+        this.state = new State(configData);
+        const lazyLoad = this.getAttribute('lazyLoad', true, false);
+        if (!lazyLoad) {
+            const networks = this.getAttribute('networks', true);
+            const wallets = this.getAttribute('wallets', true);
+            const defaultChainId = this.getAttribute('defaultChainId', true);
+            const showHeader = this.getAttribute('showHeader', true);
+            const data: IGovernanceProposal = {
+                networks,
+                wallets,
+                defaultChainId,
+                showHeader
+            }
+            if (!this.isEmptyData(data)) {
+                await this.setData(data);
+            }
+        }
+        this.loadingElm.visible = false;
+        this.isReadyCallbackQueued = false;
+        this.executeReadyCallback();
+    }
+
+    private _getActions(category?: string) {
+        const actions: any[] = [];
+        return actions;
+    }
+
+    getConfigurators() {
+        return [];
+    }
+
+    private getData() {
+        return this._data;
+    }
+
+    private async setData(data: IGovernanceProposal) {
+        this._data = data;
+        this.resetRpcWallet();
+        await this.refreshUI();
+    }
+
+    async getTag() {
+        return this.tag;
+    }
+
+    private updateTag(type: 'light' | 'dark', value: any) {
+        this.tag[type] = this.tag[type] ?? {};
+        for (let prop in value) {
+            if (value.hasOwnProperty(prop))
+                this.tag[type][prop] = value[prop];
+        }
+    }
+
+    private setTag(value: any) {
+        const newValue = value || {};
+        for (let prop in newValue) {
+            if (newValue.hasOwnProperty(prop)) {
+                if (prop === 'light' || prop === 'dark')
+                    this.updateTag(prop, newValue[prop]);
+                else
+                    this.tag[prop] = newValue[prop];
+            }
+        }
+        if (this.dappContainer)
+            this.dappContainer.setTag(this.tag);
+    }
+
+    private resetRpcWallet() {
+        this.removeRpcWalletEvents();
+        const rpcWalletId = this.state.initRpcWallet(this.defaultChainId);
+        const rpcWallet = this.state.getRpcWallet();
+        const chainChangedEvent = rpcWallet.registerWalletEvent(this, Constants.RpcWalletEvent.ChainChanged, async (chainId: number) => {
+            this.refreshUI();
+        });
+        const connectedEvent = rpcWallet.registerWalletEvent(this, Constants.RpcWalletEvent.Connected, async (connected: boolean) => {
+            this.refreshUI();
+        });
+        const data: any = {
+            defaultChainId: this.defaultChainId,
+            wallets: this.wallets,
+            networks: this.networks,
+            showHeader: this.showHeader,
+            rpcWalletId: rpcWallet.instanceId
+        };
+        if (this.dappContainer?.setData) this.dappContainer.setData(data);
+    }
+
+    private async refreshUI() {
+        await this.initializeWidgetConfig();
+    }
+
+    private initWallet = async () => {
+        try {
+            await Wallet.getClientInstance().init();
+            const rpcWallet = this.state.getRpcWallet();
+            await rpcWallet.init();
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    private initializeWidgetConfig = async () => {
+        setTimeout(async () => {
+            await this.initWallet();
+        });
+    }
+
+    private connectWallet = async () => {
+        if (!isClientWalletConnected()) {
+            if (this.mdWallet) {
+                await application.loadPackage('@scom/scom-wallet-modal', '*');
+                this.mdWallet.networks = this.networks;
+                this.mdWallet.wallets = this.wallets;
+                this.mdWallet.showModal();
+            }
+            return;
+        }
+        if (!this.state.isRpcWalletConnected()) {
+            const clientWallet = Wallet.getClientInstance();
+            await clientWallet.switchNetwork(this.chainId);
+        }
+    }
+
     render() {
         return (
-            <i-scom-dapp-container id="dappContainer"></i-scom-dapp-container>
+            <i-scom-dapp-container id="dappContainer">
+                <i-panel class={customStyles} background={{ color: Theme.background.main }}>
+                    <i-panel>
+                        <i-vstack id="loadingElm" class="i-loading-overlay">
+                            <i-vstack class="i-loading-spinner" horizontalAlignment="center" verticalAlignment="center">
+                                <i-icon
+                                    class="i-loading-spinner_icon"
+                                    image={{ url: Assets.fullPath('img/loading.svg'), width: 36, height: 36 }}
+                                />
+                                <i-label
+                                    caption="Loading..." font={{ color: '#FD4A4C', size: '1.5em' }}
+                                    class="i-loading-spinner_text"
+                                />
+                            </i-vstack>
+                        </i-vstack>
+                    </i-panel>
+                    <i-scom-tx-status-modal id="txStatusModal" />
+                    <i-scom-wallet-modal id="mdWallet" wallets={[]} />
+                </i-panel>
+            </i-scom-dapp-container>
         )
     }
 } 
